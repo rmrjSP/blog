71bcabe4b62830c033ddf934c1f75113e359bc6a
Ramraj <ramrage13@gmail.com>
2022-03-16 08:53:41 -0700

    3rd commit

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var router = express.Router();
 const articleController = require('../controllers/articleController');
-
+const commentController = require('../controllers/commentController');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/article')
 });
 router.get('/article/add', articleController.renderAddForm);
 router.post('/article/add', articleController.addArticle);
+
+router.get('/article/:articleId', articleController.displayArticle);
+router.get('/article/', articleController.displayAll );
+router.get('/article/:articleId/edit', articleController.renderEditForm);
+router.post('/article/:articleId/edit', articleController.updateArticle);
+router.get('/article/:articleId/delete', articleController.deleteArticle);
+
+router.post('/article/:articleId/comment/create', commentController.createComment);
 module.exports = router;
